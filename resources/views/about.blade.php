5d5b126e77b854c0eebe74f383517eff2bb6e8dc
Ruffa Mae Mendoza <84104-22244479@SERVER2.DOMAIN>
2024-09-21 15:51:17 +0800

    0914_04_set-the-active-navigation-link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:heading>
-        Home Page
+        About Page
     </x-slot:heading>
 
-    <h1>Hello from the Home Page.</h1>
+    <h1>Hello from the About Page.</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:heading>
-        Home Page
+        Contact Page
     </x-slot:heading>
 
-    <h1>Hello from the Home Page.</h1>
+    <h1>Hello from the Contact Page.</h1>
 </x-layout>

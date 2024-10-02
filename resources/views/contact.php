95cf597bf7343c00098cb41398ad9030cd823b0a
Mikaayyy <ruffamendoza5@gmail.com>
2024-10-02 14:26:15 +0800

    0921_08_Introduction to Migrations

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>About Page</title>
+    <title>Contact Page</title>
 </head>
 <body>
-    <h1> Hello from the About Page </h1>
+    <h1> Hello from the Contact Page </h1>
 </body>
 </html>

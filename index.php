63ad7342f8097da8cdf86176e54ead84df70a8ca
Mikaayyy <ruffamendoza5@gmail.com>
2024-10-03 12:08:07 +0800

    0921_19_Environments and Configuration Flexibility

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require 'functions.php';
 //require 'router.php';
 require 'Database.php';
 
-$db = new Database();
-$posts = $db->query("select * from post")->fetchAll(PDO::FETCH_ASSOC);
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from post")->fetchAll();
 
 dd($posts);
